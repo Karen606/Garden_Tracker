b04fc117baec4e1c7ccc7eb7105b8b69d4d465f5
Karen <karen606>
2024-10-28 18:34:06 +0400

    create tab bar

---
+++ Garden_Tracker/Garden_Tracker/Extensions/Font/Font.swift
@@ -0,0 +1,30 @@
+//
+//  Font.swift
+//  Garden_Tracker
+//
+//  Created by Karen Khachatryan on 28.10.24.
+//
+
+import UIKit
+
+extension UIFont {
+    static func angkor(size: CFloat) -> UIFont? {
+        return UIFont(name: "Angkor-Regular", size: CGFloat(size))
+    }
+    
+    static func montserratBold(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-Bold", size: CGFloat(size))
+    }
+    
+    static func montserratSemiBold(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-SemiBold", size: CGFloat(size))
+    }
+    
+    static func montserratMedium(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-Medium", size: CGFloat(size))
+    }
+    
+    static func montserratRegular(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-Regular", size: CGFloat(size))
+    }
+}
---
+++ Garden_Tracker/Garden_Tracker/Menu/MenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Garden_Tracker
+//
+//  Created by Karen Khachatryan on 28.10.24.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.font = .angkor(size: 28)
+    }
+
+
+}
+
